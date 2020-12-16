e98c870424efe21f8b0b5028e4d8790f11317eca
Ali Khan <akhanf@users.noreply.github.com>
2020-12-16 18:30:21 -0500

    Update test_run.py

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,4 +1,4 @@
 import subprocess
 
 def test_bids_01():
-    subprocess.check_call(['denoise_fmri','tests/bids_01','tests/out_01','participant','-np'])
+    subprocess.check_call(['./denoise_fmri/run.py','tests/bids_01','tests/out_01','participant','-np'])
